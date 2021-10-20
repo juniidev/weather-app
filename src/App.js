d127b4002b8a389cbabceb23664cee6462d33d1e
Henry <hms745@gmail.com>
2021-10-19 19:56:32 -0600

    Agregado de location y date

--- src/App.js
+++ src/App.js
@@ -5,12 +5,36 @@ const api = {
 }
 
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
+      "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+    let days = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
   return (
     <div className="app ">
       <main>
         <div className="search-box">
           <input
             type="text" className="search-bar" placeholder="Search..." />
+        </div>
+        <div className="location-box">
+          <div className="location">New York City, US</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">
+            15°C
+          </div>
+          <div className="weather">Sunny</div>
         </div>
       </main>
     </div>
